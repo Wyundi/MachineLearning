da4b575bd4d9f4efc2874e022989d6d24307a92c
Wyundi <wyundi8615@outlook.com>
2018-10-12 00:54:37 +0800

    fix bug

--- LinearRegression/LR_2.py
+++ LinearRegression/LR_2.py
@@ -17,37 +17,46 @@ class LinearRegression():
         self.x_extra = np.ones([self.m, 1])
         self.data_X = np.column_stack((self.x_extra, X))
         self.data_y = y
-        self.__theta = np.random.rand(self.m, 1)
-        self.__alpha = 0.01
+        self.m = self.data_X.shape[0]
+        self.n = self.data_X.shape[1]
+        self.__theta = np.random.rand(self.n, 1)
+        self.__alpha = 0.000001
+        self.iteration = 100000
         self.h = np.random.rand(self.m, 1)
         self.J = 0
-        self.J_iteration = np.zeros([1,1])
+        self.J_iteration = np.zeros(self.iteration)
         self.D = np.random.rand(self.n, 1)
-        self.temp = np.random.rand(self.m, 1)
+        self.temp = np.random.rand(self.n, 1)
 
     def modifyParam(alpha):
         self.__alpha = alpha
 
     def Hypothesis(self):
-        for i in range(self.m):
-            for j in range(self.n):
-                self.h[i] += self.__theta.T[0][j] * self.data_X[i][j]
+        self.h = np.dot(self.data_X, self.__theta)
+        # for i in range(self.m):
+        #     for j in range(self.n):
+        #         self.h[i] += self.__theta.T[0][j] * self.data_X[i][j]
 
     def CostFunction(self):
         self.Hypothesis()
+        self.J = 0
         for i in range(self.n):
             self.J += 1/(2*self.m) * np.power((self.h[i] - self.data_y[0][i]), 2)
+        # print(self.J)
 
     def Derivative(self):
         self.Hypothesis()
         for i in range(self.n):
             for j in range(self.m):
+                # print(self.h[j] - self.data_y[0][j])
                 self.D[i] += 1/self.m * (self.h[j] - self.data_y[0][j]) * self.data_X[j][i]
+                # print(self.D)
 
     def UpdateParam(self):
         self.Derivative()
-        self.temp = self.__theta - self.__alpha * self.D.T
+        self.temp = self.__theta - self.__alpha * self.D
         self.__theta = self.temp
+        # print(self.__theta)
     
 class Visualize():
     # 数据可视化
@@ -81,6 +90,9 @@ def main():
     # print(data.shape)             ->(442, 10)
     # print(target.shape)           ->(1, 442))
 
+    # Only one feature  单一变量
+    data = diabetes.data[:, np.newaxis, 2]
+
     x = data
     y = target
 
@@ -100,12 +112,27 @@ def main():
     LR.CostFunction()
     print(LR.J)
 
-    for i in range(1000):
-        LR.Derivative()
+    LR.Derivative()
+    print(LR.D.shape)
+
+    tic = time.time()
+
+    for i in range(LR.iteration):
+        LR.UpdateParam()
+        LR.CostFunction()
+        LR.J_iteration[i] = LR.J
+        if LR.J_iteration[i-1] - LR.J_iteration[i] == 0.0001:
+            print(i)
+
+    toc = time.time()
+    print(str(1000*(toc-tic)) + 'ms')
 
     PLT.plot(x_train, LR.h)
-
     PLT.scatter(x_train, y_train.T)
+
+    # x_axis = np.arange(LR.iteration)
+    # PLT.plot(x_axis, LR.J_iteration)
+
     # PLT.axis()
     PLT.cross()
     PLT.show()
